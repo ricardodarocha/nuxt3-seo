6e02badc29a607ad600df41679854b4e4a7cd201
Ricardo da Rocha <ricardodarocha@outlook.com>
2023-02-28 23:09:59 -0300

    Prod

---
+++ pages/prod/[id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{id}}</h2>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p {
+    margin: 20px 0;
+}
+</style>
